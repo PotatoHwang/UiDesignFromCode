5218bbe95d90f928f8c2bbc71b90a661541f67ae
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-07 19:44:27 +0800

    adjust subview sequence

--- UiDesignFromCode/ViewController.m
+++ UiDesignFromCode/ViewController.m
@@ -27,13 +27,28 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"painting.jpg" ofType:nil];
-    NSLog(@"path %@", path);
-    UIImage *image = [[UIImage alloc] initWithContentsOfFile:path];
+    NSLog(@"self.view %p", self.view);
+    
+    UIImage *image = [UIImage imageNamed:@"painting.jpg"];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     imageView.frame = CGRectMake(10, 10, 307.2, 409.6);
     [self.view addSubview:imageView];
+    NSLog(@"imageView %p", imageView);
     
+    UIView *blueView = [[UIView alloc] initWithFrame:CGRectMake(110, 80, 100, 100)];
+    blueView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:blueView];
+    NSLog(@"blueView %p", blueView);
+
+    NSLog(@"imageView.superview %p", imageView.superview);
+
+    [self.view bringSubviewToFront:imageView];
+    
+    for(UIView *subview in self.view.subviews)
+    {
+        NSLog(@"subview %p", subview);
+
+    }
 }
 
 - (void)didReceiveMemoryWarning
