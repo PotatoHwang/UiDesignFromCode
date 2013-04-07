bcb2166cce146e9ffadf8c8888d9e0e542012862
Peter Pan <PeterPan@Peters-MacBook-Pro.local>
2013-04-07 21:50:47 +0800

    centerUi

--- UiDesignFromCode/ViewController.m
+++ UiDesignFromCode/ViewController.m
@@ -21,27 +21,17 @@
     
 }
 
--(void)removeView1:(UIView*)targetView
-{
-    [targetView removeFromSuperview];
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
-    TestView *view1 = [[TestView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    view1.tag = 1;
-    view1.backgroundColor = [UIColor yellowColor];
-    [self.view addSubview:view1];
-    
-    TestView *view2 = [[TestView alloc] initWithFrame:CGRectMake(20, 20, 100, 100)];
-    view2.tag = 2;
-    view2.backgroundColor = [UIColor blueColor];
-    [view1 addSubview:view2];
-    
-    [self performSelector:@selector(removeView1:) withObject:view1 afterDelay:10];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 307.2, 409.6)];
+    imageView.image = [UIImage imageNamed:@"painting.jpg"];
+    [self.view addSubview:imageView];
+    imageView.center = CGPointMake(160, ([UIScreen mainScreen].bounds.size.height-20)/2);
+    NSLog(@"[UIScreen mainScreen].bounds %@", NSStringFromCGRect([UIScreen mainScreen].bounds));
 }
 
 - (void)didReceiveMemoryWarning
